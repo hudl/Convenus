2b47b2ff6ee27972967eab14b011780c1f5e7b1f
Brian Kaiser <brian.kaiser@hudl.com>
2013-12-28 14:37:59 -0500

    Basic Find Meeting room page

---
+++ Convenus/FindRoom/FindRoom.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Nancy;
+
+namespace Convenus
+{
+    public class FindRoom : NancyModule
+    {
+        public FindRoom()
+        {
+            Get["/findroom"] = _ =>
+                {
+                    return View["FindRoom.html", new { Company = Program.Options.CompanyName ?? "Convenus" }];
+                };
+
+        }
+    }
+}
