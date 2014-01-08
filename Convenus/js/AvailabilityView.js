55d77ff0c0d362c79bf650326b4884ac5f207850
Brian Kaiser <brian.kaiser@hudl.com>
2014-01-08 11:29:56 -0500

    Added pending-occupied state to a room

--- Convenus/js/AvailabilityView.js
+++ Convenus/js/AvailabilityView.js
@@ -7,19 +7,36 @@
         this.render();
     },
     onRender: function () {
-        this.$el.removeClass('unavailable available').addClass((this.model.getCurrentMeeting() != null) ? 'unavailable' : 'available');
+        var currentMeeting = this.model.getCurrentMeeting();
+        var nextMeeting = this.model.getNextMeeting();
+
+        var newClass = '';
+        if (currentMeeting != null) {
+            newClass = 'unavailable';
+        }
+        else if (nextMeeting != null && moment().add('minutes', 5).isAfter(nextMeeting.StartTime)) {
+            //if room will be blocked within 5 min - change style to indicate
+            //if you don't want to support this feature just set make pending-occupied equal to available
+            newClass = 'pending-occupied';
+        } 
+        else {
+            newClass = 'available';
+        }
+        
+        this.$el.removeClass('unavailable available pending-occupied').addClass(newClass);
     },
     serializeData: function () {
         var desc, state;
 
         var currentMeeting = this.model.getCurrentMeeting();
+        var nextMeeting = this.model.getNextMeeting();
+        
         if (currentMeeting != null) {
             //in currently meeting
             desc = 'Occupied';
             state = 'until ' + currentMeeting.EndTime.format('h:mm a'); //retrieve from current meeting item
         } else {
             desc = 'Available';
-            var nextMeeting = this.model.getNextMeeting();
             if (nextMeeting != null) {
                 state = 'until ' + nextMeeting.StartTime.format('h:mm a');//retrieve start time of next meet
             } else {
