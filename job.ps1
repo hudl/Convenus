ea18056e61435f2e96ed44020236bf05917df512
Wei Liao <wliao008@gmail.com>
2015-03-25 16:03:45 -0400

    Added Get-RoomStatus to access Convenus api

--- job.ps1
+++ job.ps1
@@ -1,4 +1,41 @@
+<#
+.Synopsis
+   Call the Convenus API to get the room status.
+.DESCRIPTION
+   Given a list of room, call the Convenus API to get the room status.
+.EXAMPLE
+   "bojackson@hudl.com", "gordie@hudl.com" | Get-RoomStatus
+#>
+function Get-RoomStatus
+{
+    [CmdletBinding()]
+    Param
+    (
+        # List of room
+        [Parameter(Mandatory=$true,
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        [string[]]
+        $Room
+    )
 
-$url = "https://api.spark.io/v1/devices/$env:SparkCoreDeviceId/led"
-$postParams = @{access_token=$env:SparkCoreAccessToken;params='LOW'}
-Invoke-WebRequest -Uri $url -Method Post -Body $postParams
+    Begin
+    {
+        $result = @{}
+    }
+    Process
+    {
+        $url = "http://calendars/api/rooms/$Room/5"
+        $status = Invoke-RestMethod $url
+        $result += @{"$Room" = $status}
+    }
+    End
+    {
+        return $result
+    }
+}
+
+#$url = "https://api.spark.io/v1/devices/$env:SparkCoreDeviceId/led"
+#$postParams = @{access_token=$env:SparkCoreAccessToken;params='LOW'}
+#Invoke-WebRequest -Uri $url -Method Post -Body $postParams
