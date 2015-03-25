4fa2fbb96572742a1d565c8c48585179ae6f0feb
Mitchel Pigsley <mpigsley@gmail.com>
2015-03-25 09:55:39 -0600

    Setup new spark route

---
+++ Convenus/Spark/Spark.cs
@@ -0,0 +1,19 @@
+﻿using Nancy;
+
+namespace Convenus.Spark
+{
+    public class Spark : NancyModule
+    {
+        public Spark()
+        {
+            Get["/spark"] = _ =>
+                {
+                    return View["Spark.html", new
+                    {
+                        SparkUserName = Program.Options.SparkUserName ?? "",
+                        SparkPassword = Program.Options.SparkPassword ?? ""
+                    }];
+                };
+        }
+    }
+}
